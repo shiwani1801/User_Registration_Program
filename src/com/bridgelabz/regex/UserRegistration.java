a29e86539b170fec7d03ad509d3690e23365587a
shiwani <shiwanipaunikar1@gmail.com>
2022-12-27 11:53:01 -0800

    [shiwani]check entered Last name is valid or not

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -20,5 +20,21 @@ public class UserRegistration
             System.out.println("It is not a valid name");
         }
     }
+    /**
+ * Method to check given last name is valid or not
+ * with minimum characters 3 and ensure first letter is captial
+ */
+public void checkLastName(String name)
+{
+    boolean isMatched = Pattern.compile("(^([A-Z]{1})[a-z]{2,})+").matcher(name).matches();
+    if(isMatched)
+    {
+        System.out.println("It is valid name");
+    }
+    else
+    {
+        System.out.println("It is not a valid name");
+    }
+}
 
 }
--- src/com/bridgelabz/regex/UserRegistrationMain.java
+++ src/com/bridgelabz/regex/UserRegistrationMain.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 
 public class UserRegistrationMain
 {
-    public static void main(String args[])
-    {
-        System.out.println("Welcome to User Registration Problem");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your first name: ");
-        String firstName = scanner.next();
+        public static void main(String args[])
+        {
+            System.out.println("Welcome to User Registration Problem");
+            Scanner scanner = new Scanner(System.in);
 
-        /**
-         * UC1
-         * Check first name
-         */
-        UserRegistration obj1 = new UserRegistration();
-        obj1.checkFirstName(firstName);
+            /**UC1
+             * Check first name
+             */
+            System.out.println("Enter your first name: ");
+            String firstName = scanner.next();
+            UserRegistration obj1 = new UserRegistration();
+            obj1.checkFirstName(firstName);
+
+            /**UC2
+             * Check last name
+             */
+            System.out.println("Enter your last name: ");
+            String lastName = scanner.next();
+            UserRegistration obj2 = new UserRegistration();
+            obj2.checkLastName(lastName);
+        }
     }
-}
